ae7e2b74cc46c9df7977cffa1ef6f322cd904b9e
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 18:37:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,5 +50,5 @@ def sum_array(int_array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element << "s" unless index == 1}
+  array.each_with_index.collect{|element, index| index == 1 ? element : element << "s"}
 end
